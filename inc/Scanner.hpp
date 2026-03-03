5a0b89a4841cfebe727c51b576034d576a34ade1
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2026-03-03 15:52:14 +0100

    refactor(config scanner): changes from using token pointer list to using token list

--- inc/Scanner.hpp
+++ inc/Scanner.hpp
@@ -8,10 +8,10 @@ class Scanner {
 public:
   explicit Scanner(const char *File);
   ~Scanner(void);
-  const std::list<const Token *> &getTokens(void) const;
+  const std::list<Token> &getTokens(void) const;
 
 private:
-  std::list<const Token *> _tokens;
+  std::list<Token> _tokens;
   void addEof(const size_t Line);
   void addNewline(const size_t Line);
   void scanLine(const size_t Number, const std::string &Str);
--- src/config/parsing/Scanner.cpp
+++ src/config/parsing/Scanner.cpp
@@ -21,11 +21,7 @@ size_t Token::getLine(void) const {
   return _line;
 }
 
-Scanner::~Scanner() {
-  for (std::list<const Token *>::iterator it = _tokens.begin();
-       it != _tokens.end(); ++it)
-    delete *it;
-}
+Scanner::~Scanner() { }
 
 Scanner::Scanner(const char *File) {
   std::ifstream inf(File);
@@ -47,38 +43,35 @@ Scanner::Scanner(const char *File) {
 }
 
 void Scanner::addEof(const size_t Line) {
-  const Token *const tkn = new Token(Line, TokenType::Eof);
-  _tokens.push_back(tkn);
+  _tokens.push_back(Token(Line, TokenType::Eof));
 }
 
 void Scanner::addNewline(const size_t Line) {
-  const Token *const tkn = new Token(Line, TokenType::Newline);
-  _tokens.push_back(tkn);
+  _tokens.push_back(Token(Line, TokenType::Newline));
 }
 
 void Scanner::scanLine(const size_t Number, const std::string &Str) {
   std::string::const_iterator it = Str.begin();
   std::string::const_iterator itTmp;
   while (it != Str.end()) {
-    const Token *const tkn = new Token(Number, Str, it, itTmp);
-    _tokens.push_back(tkn);
+    _tokens.push_back(Token(Number, Str, it, itTmp));
     it = itTmp;
   }
 }
 
 std::ostream &operator<<(std::ostream &Os, const Scanner &Scan) {
   size_t line = 0;
-  for (std::list<const Token *>::const_iterator it = Scan.getTokens().begin();
+  for (std::list<Token>::const_iterator it = Scan.getTokens().begin();
        it != Scan.getTokens().end(); ++it) {
-    if ((**it).getLine() > line) {
-      line = (**it).getLine();
+    if ((*it).getLine() > line) {
+      line = (*it).getLine();
       Os << "\n########### Line " << line << " ###########\n";
     }
-    Os << **it;
+    Os << *it;
   }
   return Os;
 }
 
-const std::list<const Token *> &Scanner::getTokens(void) const {
+const std::list<Token> &Scanner::getTokens(void) const {
   return _tokens;
 }
