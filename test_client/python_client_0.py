7c7e10cd1475071f57b38b1e4ee6e3df69f72c9e
Hilary Allison <165587952+hilary-allison@users.noreply.github.com>
2026-03-06 15:48:59 +0100

    feature(test client): test fragmented requests

--- test_client/python_client_0.py
+++ test_client/python_client_0.py
@@ -9,11 +9,14 @@ ip = 'localhost'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket object
 s.connect((ip, port))
-request = "GET /home/hallison/webserv/Makefile HTTP/1.0\r\n\r\n"
+request = "GET /home/hallison/webserv/randomHTMLfile.html HTTP/1.0\r\n\r\n"
+request_half_0 = "GET "
+request_half_1 = "/home/hallison/webserv/randomHTMLfile.html HTTP/1.0\r\n\r\n"
 
-s.send(request.encode())
+s.send(request_half_0.encode())
+s.send(request_half_1.encode())
 data = s.recv(1024).decode()
 print(data)
 # print(s.recv(1024))
 
-s.close()
+#s.close()
