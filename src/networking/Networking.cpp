370ed5c09598f56f5d6172b914827543f60cfc0e
Hilary Allison <165587952+hilary-allison@users.noreply.github.com>
2026-03-11 14:48:32 +0100

    feature(Server class): create Server class for use in networking

--- src/networking/Networking.cpp
+++ src/networking/Networking.cpp
@@ -6,16 +6,32 @@
 /*   By: hallison <hallison@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 17:52:19 by hallison          #+#    #+#             */
-/*   Updated: 2026/02/24 14:06:03 by hallison         ###   ########.fr       */
+/*   Updated: 2026/03/11 14:42:56 by hallison         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Networking.hpp"
+#include "Server.hpp"
 // #define _GNU_SOURCE // for extra poll() macros // defined elsewhere? Compiler
 // complains
 #include <netdb.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+
+/*
+	TODO when connecting to config
+  	get list of interface::port pairs from config parsing
+  	for EACH of them...
+  		generate addrinfo
+		get socket
+		free addrinfo
+		set socket to listen
+*/
+
+/*
+void generateDummyWebsite(std::string ip; Type type, unsigned int port) {
+}
+*/
 
 void networking::start(void) {
 
@@ -23,7 +39,6 @@ void networking::start(void) {
   int sock;
   sock = getServerSocket(serverInfo);
   freeaddrinfo(serverInfo);
-
   setToListen(sock);
 
   // signal handling goes here
