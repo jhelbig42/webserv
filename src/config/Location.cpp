f08db44a0142f448294afab1b3b9ab2a8b5a6ce8
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2026-04-01 14:04:24 +0200

    feat(config parsing): almost fully implements class Location
    << operator overload for writing to streams missing
    not compiled or tested yet

--- src/config/Location.cpp
+++ src/config/Location.cpp
@@ -21,41 +21,69 @@ Location &Location::operator=(const Location &Other) {
 Location::~Location() {
 }
 
-explicit Location::Location(const std::string &Path): _path(Path) {
+explicit Location::Location(const std::string &Path)
+    : _setMembers(0), _path(Path) {
 }
 
-void Location::setReturn(const std::string &RootDir) {
+void Location::setReturn(const std::string &Code, const std::string &Uri) {
+  _redirect.code = Code;
+  _redirect.uri = Uri;
+  _setMembers |= Location::Redirect;
 }
 
 void Location::addAllow(const HttpMethod Method) {
+  _allow |= Method;
+  _setMembers |= Location::Allow;
 }
 
 void Location::setRedirect(const std::string &Redirect) {
+  _redirect = Redirect;
+  _setMembers |= Location::Redirect;
 }
 
 void Location::setCgi(const std::string &Cgi) {
+  _cgi = Cgi;
+  _setMembers |= Location::Cgi;
 }
 
-void Location::getReturn(int &Code, std::string &Url) {
+bool Location::getReturn(std::string &Code, std::string &Url) {
+  if (!isSetRedirect())
+    return false;
+  Code = _redirect.code;
+  Uri = _redirect.uri;
+  return true;
 }
 
 bool Location::isAllowed(const HttpMethod Method) {
+  return _allow & Method;
 }
 
-void Location::getRedirect(std::string &Redirect) {
+bool Location::getRedirect(std::string &Redirect) {
+  if (!isSetRedirect())
+    return false;
+  Redirect = _redirect;
+  return true;
 }
 
-void Location::getCgi(std::string &Cgi) {
+bool Location::getCgi(std::string &Cgi) {
+  if (!isSetCgi())
+    return false;
+  Cgi = _cgi;
+  return true;
 }
 
 bool Location::isSetReturn(void) {
+  return _setMembers & Location::Return;
 }
 
-bool Location::isAddAllow(void) {
+bool Location::isSetAllow(void) {
+  return _setMembers & Location::Allow;
 }
 
 bool Location::isSetRedirect(void) {
+  return _setMembers & Location::Redirect;
 }
 
 bool Location::isSetCgi(void) {
+  return _setMembers & Location::Cgi;
 }
