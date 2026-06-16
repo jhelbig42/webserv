72dffe97d511c4acf190c319a7963bfcfc69cb8c
Alrik Neumann <131678067+alneuma@users.noreply.github.com>
2026-06-16 17:11:47 +0200

    chore: set default log level from Info to Warning

--- src/config/ConfigDefaults.hpp
+++ src/config/ConfigDefaults.hpp
@@ -10,9 +10,9 @@
 /// \brief sets the default log level
 ///
 /// should be one of the following:
-/// logging::DEBUG, logging::INFO, logging::WARNING, logging::ERROR
+/// logging::Debug, logging::Info, logging::Warning, logging::Error
 #ifndef LOG_LEVEL
-#define LOG_LEVEL logging::Info
+#define LOG_LEVEL logging::Warning
 #endif // DEFAULT_LOG_LEVEL
 
 #ifndef LOG_COLORED
